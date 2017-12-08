27c8c86e71dc2a8431f7165f6c37ecb06a5b56b0
Sacha GRASSO <s.grasso@gop.link>
2017-12-08 21:01:12 +0100

    nothing

--- src/global-modules-indexer/index.js
+++ src/global-modules-indexer/index.js
@@ -6,18 +6,18 @@ let path = require('path');
 let indexCtrl = {};
 
 function startRecursiveCheck(path) {
-    fs.readdirSync(path).forEach(e => {
-      let name = e.replace('.js', '');
-      indexCtrl[name] = {};
+  fs.readdirSync(path).forEach(e => {
+    let name = e.replace('.js', '');
+    indexCtrl[name] = {};
 
-      fs.statSync(path +'/'+ e).isFile() ?  indexCtrl[name] = require(path + '/' + e) : nextChecks(name, path + '/' + e);
-    });
-  };
-  function nextChecks(name, path) {
-    fs.readdirSync(path).forEach(e => {
-      fs.statSync(path +'/'+ e).isFile() ?  indexCtrl[name] = require(path + '/' + e) : nextChecks(name, path + '/' + e);
-    });
-  };
+    fs.statSync(path + '/' + e).isFile() ? indexCtrl[name] = require(path + '/' + e) : nextChecks(name, path + '/' + e);
+  });
+};
+function nextChecks(name, path) {
+  fs.readdirSync(path).forEach(e => {
+    fs.statSync(path + '/' + e).isFile() ? indexCtrl[name] = require(path + '/' + e) : nextChecks(name, path + '/' + e);
+  });
+};
 
 function GlobalModulesIndexer(context, dirName) {
   let absolutePath = path.join(context, dirName);
