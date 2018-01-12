12d0d2d82258cec4a7a71e2633cf12c545ed46ae
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2018-01-12 15:15:54 +0100

    fixed test and cleaned package.json

--- test/mapper.spec.ts
+++ test/mapper.spec.ts
@@ -4,109 +4,231 @@
  * Import test suite
  */
 import 'mocha';
-import * as express from 'express';
-import * as sinon from 'sinon';
-import {request} from 'https';
+import {expect} from 'chai';
+import * as supertest from 'supertest';
 
-/**
- * Import chai library
- */
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+import Router from '../src/route-mapper/map';
 
 let customConfig = {
   getAll: 'listAll',
   getOne: 'findOne'
 };
 
-import mapper from '../src/route-mapper/map';
-let map = mapper(__dirname, customConfig, './mocks');
+let router = Router(__dirname, customConfig, './mocks')
 
-/**
- * Set up express stub and tested module stub
- */
-
-
-chai.use(chaiHttp);
-const expect = chai.expect;
-
-beforeEach(() => {})
+const createStubServer = require('./stub/stubServer');
+let server, request;
 
 /**
  * Start testing
  */
 
 describe('RouteMapper', () => {
-  it('should be a function', () => {
-    console.log(map.stack[0].route)
-    expect(typeof map).to.equal('function');
-    
+  server = createStubServer();
+  request = supertest(server);
+
+  it('should be a function setting up routing', () => {
+    expect(typeof Router).to.equal('function');
   });
 
-  it('should return a router object', () => {
-    expect(typeof map).to.equal('function');
-  });
+  describe('handling errors during mapping', () => {
+    it('should return an error 500 if request is not handled', (done) => {
+      request
+        .options('/test/request/not/handled')
+        .expect(500)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(500);
+          expect(res.body).to.deep.equal('Request not handled, it must be one of GET, POST, PUT, PATCH, or DELETE');
+          done();
+        });
+    });
 
-  xdescribe('handling errors during mapping', () => {
-    it('should return an error 500 if request is not handled', () => {
-
+    it('should return 404 if url is not matching anything', (done) => {
+      request
+        .get('/this/is/not/valid')
+        .expect(404)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(404);
+          expect(res.res.statusMessage).to.deep.equal('Not Found');
+          done();
+        });
     });
   })
-  xdescribe('skippin this part for now', () => {
-    it('should return 404 if url is not matching anything', () => {
 
-    });
-  })
-
-  xdescribe('mapping to controller with methods correctly named', () => {
-    it('should return a collection', () => {
-
-    });
-
-    it('should return a single document', () => {
-
-    });
-
-    it('should post a single document', () => {
-
+  describe('mapping to controller with methods correctly named', () => {
+    it('should return a collection', (done) => {
+      request
+        .get('/test/index')
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(200);
+          done();
+        });
     });
 
-    it('should update a single document', () => {
-
+    it('should return a single document', (done) => {
+      request
+        .get('/test/index/param1')
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(200);
+          done();
+        });
     });
 
-    it('should update a single document', () => {
-
+    it('should post a single document', (done) => {
+      request
+        .post('/test/index')
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(200);
+          done();
+        });
     });
 
-    it('should delete a single document', () => {
+    it('should update a single document', (done) => {
+      request
+        .put('/test/index')
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(200);
+          done();
+        });
+    });
 
+    it('should update part of a single document', (done) => {
+      request
+        .patch('/test/index')
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(200);
+          done();
+        });
+    });
+
+    it('should delete a single document', (done) => {
+      request
+        .delete('/test/index')
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(200);
+          done();
+        });
     });
   });
 
-  xdescribe('mapping to controller with methods incorrectly named', () => {
-    it('should return an error when fetching a collection', () => {
-
+  describe('mapping to controller with methods incorrectly named', () => {
+    it('should return an error when fetching a collection', (done) => {
+      request
+        .get('/test/beta')
+        .expect(500)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(500);
+          expect(res.error.status).to.deep.equal(500);
+          expect(res.error.text).to.deep.equal(`Trying to use a method not implemented in the controller like advised in the docs of the router.
+Go have a look at https://github.com/gaspaonrocks/universal-node-router/blob/master/readme.md for more info.
+If it is still not working, post an issue.`);
+          done();
+        });
     });
 
-    it('should return an error when fetching a single document', () => {
-
+    it('should return an error when fetching a single document', (done) => {
+      request
+        .get('/test/beta/param1')
+        .expect(500)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(500);
+          expect(res.error.status).to.deep.equal(500);
+          expect(res.error.text).to.deep.equal(`Trying to use a method not implemented in the controller like advised in the docs of the router.
+Go have a look at https://github.com/gaspaonrocks/universal-node-router/blob/master/readme.md for more info.
+If it is still not working, post an issue.`);
+          done();
+        });
     });
 
-    it('should return an error when posting a single document', () => {
-
+    it('should return an error when posting a single document', (done) => {
+      request
+        .post('/test/beta')
+        .expect(500)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(500);
+          expect(res.error.status).to.deep.equal(500);
+          expect(res.error.text).to.deep.equal(`Trying to use a method not implemented in the controller like advised in the docs of the router.
+Go have a look at https://github.com/gaspaonrocks/universal-node-router/blob/master/readme.md for more info.
+If it is still not working, post an issue.`);
+          done();
+        });
     });
 
-    it('should return an error when updating a single document', () => {
-
+    it('should return an error when updating a single document', (done) => {
+      request
+        .put('/test/beta/param1')
+        .expect(500)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(500);
+          expect(res.error.status).to.deep.equal(500);
+          expect(res.error.text).to.deep.equal(`Trying to use a method not implemented in the controller like advised in the docs of the router.
+Go have a look at https://github.com/gaspaonrocks/universal-node-router/blob/master/readme.md for more info.
+If it is still not working, post an issue.`);
+          done();
+        });
     });
 
-    it('should return an error when updating a single document', () => {
-
+    it('should return an error when updating part of a single document', (done) => {
+      request
+        .patch('/test/beta/param1')
+        .expect(500)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(500);
+          expect(res.error.status).to.deep.equal(500);
+          expect(res.error.text).to.deep.equal(`Trying to use a method not implemented in the controller like advised in the docs of the router.
+Go have a look at https://github.com/gaspaonrocks/universal-node-router/blob/master/readme.md for more info.
+If it is still not working, post an issue.`);
+          done();
+        });
     });
 
-    it('should return an error when deleting a single document', () => {
-
+    it('should return an error when deleting a single document', (done) => {
+      request
+        .delete('/test/beta/param1')
+        .expect(500)
+        .end((err, res) => {
+          if (err) done(err)
+          expect(err).to.be.null
+          expect(res.status).to.deep.equal(500);
+          expect(res.error.status).to.deep.equal(500);
+          expect(res.error.text).to.deep.equal(`Trying to use a method not implemented in the controller like advised in the docs of the router.
+Go have a look at https://github.com/gaspaonrocks/universal-node-router/blob/master/readme.md for more info.
+If it is still not working, post an issue.`);
+          done();
+        });
     });
   });
+  server.close();
 });
