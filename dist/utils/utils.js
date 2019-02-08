09469561ef67c4f748d2d14bfc50cd45b383bdb0
sacha.grasso <sacha.grasso@ext.cdiscount.com>
2019-02-08 17:21:33 +0100

    functionnal programming in utils file

--- dist/utils/utils.js
+++ dist/utils/utils.js
@@ -4,20 +4,10 @@ exports.default = {
     hasReqParams: (path) => {
         return path.split('/').filter(e => e.length > 0).length > 1;
     },
-    reqParamsOptions: (path) => {
-        let config = {
-            ctrl: '',
-            url: '/'
-        };
-        let array = path.split('/').filter(e => e.length > 0);
-        config.ctrl = array[0];
-        config.url += array[0];
-        for (let i = 1, len = array.length; i < len; i++) {
-            config[`param${i}`] = array[i];
-            config.url += `/:param${i}`;
-        }
-        return config;
-    },
+    reqParamsOptions: (path) => path
+        .split('/')
+        .filter(e => e.length > 0)
+        .reduce((accumulator, currentValue, i) => i === 0 ? Object.assign({}, accumulator, { ctrl: currentValue, url: accumulator.url + `/${currentValue}` }) : Object.assign({}, accumulator, { [`param${i}`]: currentValue, url: accumulator.url + `/:param${i}` }), { ctrl: '', url: '' }),
     requireMyTsFile: (fileName, filePath) => {
         return fileName.match(/.ts$/) ? require(filePath + '/' + fileName).default : require(filePath + '/' + fileName);
     }
